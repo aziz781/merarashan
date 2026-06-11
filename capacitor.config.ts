ef9553670051e47bc50ef7bf5cdd9005504d2dd0
Abdul Aziz (A575072) <seeazee@gmail.com>
2026-06-11 21:38:00 +0100

    Comment out server configuration
    Comment out the server configuration in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.6fdf31a43e344895b7822f7c14c350ba',
   appName: 'merarashan',
   webDir: 'dist',
-  server: {
-    url: 'https://6fdf31a4-3e34-4895-b782-2f7c14c350ba.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
+  // server: {
+  //   url: 'https://6fdf31a4-3e34-4895-b782-2f7c14c350ba.lovableproject.com?forceHideBadge=true',
+  //   cleartext: true,
+  // },
 };
 
 export default config;
